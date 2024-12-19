0e110f847832271c2bc0cbbc3c7728cd9ad4a9ff
Tereami <undress.my.future@gmail.com>
2024-12-19 20:35:49 +0300

    first commit

---
+++ App.cs
@@ -0,0 +1,54 @@
+﻿#region License
+/*Данный код опубликован под лицензией Creative Commons Attribution-ShareAlike.
+Разрешено использовать, распространять, изменять и брать данный код за основу для производных в коммерческих и
+некоммерческих целях, при условии указания авторства и если производные лицензируются на тех же условиях.
+Код поставляется "как есть". Автор не несет ответственности за возможные последствия использования.
+Зуев Александр, $year$, все права защищены.
+This code is listed under the Creative Commons Attribution-ShareAlike license.
+You may use, redistribute, remix, tweak, and build upon this work non-commercially and commercially,
+as long as you credit the author by linking back and license your new creations under the same terms.
+This code is provided 'as is'. Author disclaims any implied warranty.
+Zuev Aleksandr, $year$, all rights reserved.*/
+#endregion
+#region Usings
+using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+[assembly: System.Reflection.AssemblyVersion("1.0.*")]
+namespace $safeprojectname$
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string assemblyPath = typeof(App).Assembly.Location;
+
+            string tabName = "TEST";
+            try { application.CreateRibbonTab(tabName); }
+            catch { }
+			
+			string projectName = nameof($safeprojectname$);
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, $"{projectName} panel");
+			string commandName = nameof(Command);
+            _ = panel.AddItem(new PushButtonData(
+                commandName,
+                commandName,
+                assemblyPath,
+                $"{projectName}.{commandName}")
+                ) as PushButton;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Command.cs
@@ -0,0 +1,34 @@
+﻿#region License
+/*Данный код опубликован под лицензией Creative Commons Attribution-ShareAlike.
+Разрешено использовать, распространять, изменять и брать данный код за основу для производных в коммерческих и
+некоммерческих целях, при условии указания авторства и если производные лицензируются на тех же условиях.
+Код поставляется "как есть". Автор не несет ответственности за возможные последствия использования.
+Зуев Александр, $year$, все права защищены.
+This code is listed under the Creative Commons Attribution-ShareAlike license.
+You may use, redistribute, remix, tweak, and build upon this work non-commercially and commercially,
+as long as you credit the author by linking back and license your new creations under the same terms.
+This code is provided 'as is'. Author disclaims any implied warranty.
+Zuev Aleksandr, $year$, all rigths reserved.*/
+#endregion
+#region Usings
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+
+namespace $safeprojectname$
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            return Result.Succeeded;
+        }
+    }
+}
